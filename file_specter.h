e190caaa3ecfaca8a90afd1da5cfec73c4522279
OmarAzizi <oalazizi75@gmail.com>
2024-02-04 14:47:26 +0300

    added exit codes and threads

--- file_specter.h
+++ file_specter.h
@@ -13,8 +13,10 @@
 #include <sys/inotify.h>
 #include <libnotify/notify.h>
 #include <libnotify/notification.h>
+#include <pthread.h>
 
 // exit codes
+#define     THREAD_ERROR                   -1
 #define     EXIT_SUCCESS                    0
 #define     EXIT_FALIURE                    1
 #define     EXIT_ERROR_INIT_INOTIFY         2
@@ -22,12 +24,12 @@
 #define     EXIT_ERROR_BASE_PATH_NULL       4
 #define     EXIT_ERROR_READ_INOTIFY         5
 #define     EXIT_ERROR_INIT_LIBNOTIFY       6
+#define     EXIT_ERROR_BUFFER_OVERFLOW      7
 
 char* get_filename_from_path(char*);
 void run_filespecter(char**);
 void handle_events(char*);
-void check(const struct inotify_event*, char*);
-void notify(char*, char*);
+void* notify(void*);
 void signal_handler(int);
 
 #endif
